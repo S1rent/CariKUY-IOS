0f11fa9c8ffae92ca708a8008f056b5c443b9484
philip.prayitno <philip.prayitno@binus.edu>
2021-06-16 21:56:22 +0700

    Init/first_commit

---
+++ Pods/Target Support Files/Pods-CariKUY-CariKUYUITests/Pods-CariKUY-CariKUYUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CariKUY_CariKUYUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CariKUY_CariKUYUITests
+@end
---
+++ Pods/Target Support Files/Pods-CariKUY/Pods-CariKUY-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CariKUY : NSObject
+@end
+@implementation PodsDummy_Pods_CariKUY
+@end
